6d2439841106e93f5d14840f089500e41b9da637
Evgeniy <durasell96ms@gmail.com>
2023-12-24 12:55:01 +0300

    feat: add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,56 +13,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
 
-//   if (!Array.isArray(arr)) {
-//     throw new Error("'arr' parameter must be an instance of the Array!");
-//   }
-//   let result = []; 
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  let result = []; 
   
-//   arr.forEach((item, index) => { 
-//     const typeOfCurrentItem = typeof item;
-//     const typeOfPrevItem = typeof arr[index - 1];
-//     const typeOfSecondPrevItem = typeof arr[index - 2];
-//     const typeOfThirdPrevItem = typeof arr[index - 3];
-//     if (typeOfCurrentItem !== 'string' && typeOfPrevItem !== 'string') {
-//       result.push(item);
-//     } else if (typeOfCurrentItem === 'string' && item !== "--discard-next" && item !== "--discard-prev" && item !== "--double-next" && item !== "--double-prev") {
-//       result.push(item);
-//     } else { 
-//       if (typeOfCurrentItem !== 'string' && typeOfPrevItem === 'string') {
-//         if (arr[index - 1] === '--double-next') {
-//           result.push(item, item);
-//         } else if (arr[index - 1] === '--discard-next') {
+  arr.forEach((item, index) => { 
+    const typeOfCurrentItem = typeof item;
+    const typeOfPrevItem = typeof arr[index - 1];
+    const typeOfSecondPrevItem = typeof arr[index - 2];
+    const typeOfThirdPrevItem = typeof arr[index - 3];
+    if (typeOfCurrentItem !== 'string' && typeOfPrevItem !== 'string') {
+      result.push(item);
+    } else if (typeOfCurrentItem === 'string' && item !== "--discard-next" && item !== "--discard-prev" && item !== "--double-next" && item !== "--double-prev") {
+      result.push(item);
+    } else { 
+      if (typeOfCurrentItem !== 'string' && typeOfPrevItem === 'string') {
+        if (arr[index - 1] === '--double-next') {
+          result.push(item, item);
+        } else if (arr[index - 1] === '--discard-next') {
           
-//         } else if (arr[index - 1] === '--discard-prev') {
-//           if (typeOfThirdPrevItem === 'string' && arr[index - 3] === '--discard-next') {
-//           } else {
-//             result.pop();
-//           }
-//           result.push(item);
-//         } else if (arr[index - 1] === '--double-prev') {
-//           if (typeOfThirdPrevItem === 'string' && arr[index - 3] === '--discard-next') {
+        } else if (arr[index - 1] === '--discard-prev') {
+          if (typeOfThirdPrevItem === 'string' && arr[index - 3] === '--discard-next') {
+          } else {
+            result.pop();
+          }
+          result.push(item);
+        } else if (arr[index - 1] === '--double-prev') {
+          if (typeOfThirdPrevItem === 'string' && arr[index - 3] === '--discard-next') {
           
-//           } else if (typeOfThirdPrevItem === 'string' && arr[index - 3] === '--double-next') {
-//             if (arr[index - 2] && typeOfSecondPrevItem !== 'string') {
-//               result.push(arr[index - 2]);
-//             }
-//           } else {
-//             if (arr[index - 2] && typeOfSecondPrevItem !== 'string') {
-//               result.push(arr[index - 2]);
-//             }
-//           }
-//           result.push(item);
-//         }
-//       } else {
+          } else if (typeOfThirdPrevItem === 'string' && arr[index - 3] === '--double-next') {
+            if (arr[index - 2] && typeOfSecondPrevItem !== 'string') {
+              result.push(arr[index - 2]);
+            }
+          } else {
+            if (arr[index - 2] && typeOfSecondPrevItem !== 'string') {
+              result.push(arr[index - 2]);
+            }
+          }
+          result.push(item);
+        }
+      } else {
        
-//       }
-//     }
-//   });
-//   return result;
+      }
+    }
+  });
+  return result;
 }
 module.exports = {
   transform
